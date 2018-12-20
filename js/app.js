798162f2acda93536892656915632569d309cc14
Suresh Thomas <suresh.thomas@yahoo.com>
2018-12-20 23:30:52 +0000

    First submit by Suresh working game, but just working and crude

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@
  *
  *
  */
-let tileCollection; /***collection of all tiles****/
-let lockBoard = false;
+/***collection of all tiles****/
+let tileCollection;
+// Lock the game board until system is checking the match and executing time out. This will avoid continous clicking. just boolean bypass
+let lockGameBoard = false;
+//These are the first 2 tiles
 let firstTile, secondTile;
 let hasFlippedTile = false;
 const displayTimeOut = 1500;
+let  numOfMoves=0;
+
+
 
 //List of all symbols/tiles - they are font-awesome classes. This is the only magic string
 const tiles = [
@@ -27,14 +33,10 @@ const tiles = [
 
 // Number of tiles - this is derived dynamically to make sure that we don't hardcode the number of tiles
 //Removing magic strings
-
-const tileCollectionIndices = Array.apply(null,{length:tiles.length}).map(Number.call,Number);
-
+const tileCollectionIndices = Array.apply(null, {length: tiles.length}).map(Number.call, Number);
 //Double up the array to make sure that there are pair of each tile
 tileCollectionIndices.push.apply(tileCollectionIndices, tileCollectionIndices);
-
-const  numberOfTiles = tileCollectionIndices.length;
-
+const numberOfTiles = tileCollectionIndices.length;
 /***********
  * Entry point Paint the Game Board with tile collection
  * This is called once automatically when user open the page
@@ -47,31 +49,28 @@ paintGameGameBoard();
  *
  * @returns {DocumentFragment}
  */
-function createTileAssemby(){
-    var fragment = document.createDocumentFragment();
-
+function createTileAssemby() {
+    let fragment = document.createDocumentFragment();
     let list = document.createElement('ul');
-    list.className="deck";
-
-    for(i=0; i<numberOfTiles;i++) {
+    list.className = "deck";
+    for (i = 0; i < numberOfTiles; i++) {
         let listItem = document.createElement('li');
         listItem.classList.add('card');
         let indexNumber = tileCollectionIndices[i];
-       //HtML 5 capability to set data* custom attributes
-        listItem.dataset.indexNumber=indexNumber;
+        //HtML 5 capability to set data* custom attributes
+        listItem.dataset.indexNumber = indexNumber;
         textItem = document.createElement('i');
-        textItem.classList.add('fa',tiles[indexNumber]);
+        textItem.classList.add('fa', tiles[indexNumber]);
         listItem.appendChild(textItem);
         list.appendChild(listItem);
     }
     fragment.appendChild(list);
     return fragment;
-
-
 }
+
 //Shuffle deck
 //This inserts within the div
-function layTilesOnGameBoard(frag){
+function layTilesOnGameBoard(frag) {
     //Get the container inside which we will draw the game tile board
     // There could be more div elements , setting an ID may be the best way to uniquely identify the div
     const container = document.getElementById('mosaic');
@@ -81,41 +80,42 @@ function layTilesOnGameBoard(frag){
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    resetBoard();
+    resetGameBoard();
     // Build the entire board on fragment as per Udacity performance section
-
     container.appendChild(frag);
-
 }
 
+function clearTimer() {
+   gametimer.stop();
+   gametimer.reset();
+
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ *
+ * set up the event listener for a card. If a card is clicked:
  */
-function paintGameGameBoard(){
+function paintGameGameBoard() {
+
+    numOfMoves=0;
+    window.requestAnimationFrame(setMoves);
     shuffle(tileCollectionIndices);
+    clearTimer();
     var tileAssembly = createTileAssemby();
     layTilesOnGameBoard(tileAssembly);
     //select tiles into array
     tileCollection = document.querySelectorAll('.card');
-
-// set up the event listener for a card. If a card is clicked:
-
+    // set up the event listener for a card. If a card is clicked:
     tileCollection.forEach(tile => tile.addEventListener('click', toggleTile));
-    //console.log(tileCollection);
-//let isTileFolded = false;
-  // lockBoard = false;
-    //let firstTile, secondTile;
 }
-
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -123,77 +123,83 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
-
-/**
- * An ES6 version
- * @param {Array} array items An array containing the items.
- * Todo I need to optimise the function as destructuring seems to take some performance. when i checked it !
+/*********
+ * This is the event target executed when you click on button
  */
-function shuffleES6(array) {
-
-    for (let temporaryValue = array.length - 1; temporaryValue > 0; temporaryValue--) {
-        const randomIndex = Math.floor(Math.random() * (temporaryValue + 1));
-        [array[temporaryValue], array[randomIndex]] = [array[randomIndex], array[temporaryValue]];
-    }
-    return array;
-
-}
-
-
-
-function toggleTile(){
-   // console.log("toggled");
-    if(lockBoard ) return;
-    if(this === firstTile) return;
-
+function toggleTile() {
+    if (lockGameBoard) return;
+    if (this === firstTile) return;
+    gametimer.start();
+    numOfMoves++;
     this.classList.add('show');
     this.classList.add('open');
-
-
-    if(!hasFlippedTile){
+    if (!hasFlippedTile) {
         hasFlippedTile = true;
         firstTile = this;
         return;
     }
     secondTile = this;
-    checkForMatch();
+    doTheOpenedPairOfTilesMatch()?disableUserinteractionOnMatchedTiles() : unToggleTiles();
 }
 
-function checkForMatch() {
-    let isMatch = firstTile.dataset.indexNumber === secondTile.dataset.indexNumber;
-
-    isMatch ? disableTIes() : unflipTiles();
+/**- if the list already has another card, check to see if the two cards match
+ * if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ */
+function doTheOpenedPairOfTilesMatch() {
+    //I have set an html5 data*** custom attribute to element
+    return firstTile.dataset.indexNumber === secondTile.dataset.indexNumber;
 }
 
-function disableTIes() {
+/******
+ * Remove listener from the cars which are matched
+ *************/
+function disableUserinteractionOnMatchedTiles() {
     firstTile.removeEventListener('click', toggleTile);
+    firstTile.className = 'card match';
     secondTile.removeEventListener('click', toggleTile);
+    secondTile.className = 'card match';
+    //Reset temp variable
 
-    resetBoard();
+    resetGameBoard();
 }
-
-function unflipTiles() {
-    lockBoard = true;
-
+///Fold the tile with - but on a times for user to review
+function unToggleTiles() {
+    lockGameBoard = true;
     setTimeout(() => {
-
-
         firstTile.classList.remove('show');
         firstTile.classList.remove('open');
         secondTile.classList.remove('show');
         secondTile.classList.remove('open');
-
-       resetBoard();
+        resetGameBoard();
     }, displayTimeOut);
 }
-
-function resetBoard() {
-    [hasFlippedTile, lockBoard] = [false, false];
+//Number of clicks
+function resetGameBoard() {
+    [hasFlippedTile, lockGameBoard] = [false, false];
     [firstTile, secondTile] = [null, null];
 }
+
+function setMoves() {
+    document.getElementById('moveCounter').innerHTML=numOfMoves;
+    window.requestAnimationFrame(setMoves);
+}
+//window.requestAnimationFrame(setMoves);
+/*******
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+*************/
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -202,6 +208,6 @@ function resetBoard() {
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + TODO increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + TODO if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
